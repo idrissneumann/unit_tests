3e0580f72e9c1319a0e047a8c2cc52ef0ebf32d5
Idriss Neumann <idriss.neumann@comwork.io>
2021-07-08 10:41:47 +0100

    Optimize docker build

--- ci/tests.sh
+++ ci/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd todoapi
-docker-compose up --force-recreate --build todo_unit_tests
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --force-recreate --build todo_unit_tests
 cd -
